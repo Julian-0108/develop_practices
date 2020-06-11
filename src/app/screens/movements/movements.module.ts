dacaeb00f44093a06bb125f96ed0e4050c562d78
Camilo Chavarriaga <yulianyccr@gmail.com>
2020-06-11 12:32:21 -0500

    Add example table for movements report

--- src/app/screens/movements/movements.module.ts
+++ src/app/screens/movements/movements.module.ts
@@ -4,12 +4,24 @@ import { CommonModule } from '@angular/common';
 import { MovementsRoutingModule } from './movements-routing.module';
 import { MovementsComponent } from './movements.component';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+
 
 @NgModule({
   declarations: [MovementsComponent],
   imports: [
     CommonModule,
-    MovementsRoutingModule
+    MovementsRoutingModule,
+
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatSortModule
   ]
 })
 export class MovementsModule { }
