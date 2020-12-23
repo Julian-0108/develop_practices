aa40b7c4c5e034505f5cfc0348091a48c8b09725
Wilmer Contreras <wilmer.152.contreras@gmail.com>
2020-12-22 19:39:05 -0500

    Feat: home component styles was updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home'},
   { path: 'login', loadChildren: () => import('./screens/login/login.module').then(m => m.LoginModule) },
   { path: 'home', loadChildren: () => import('./screens/home/home.module').then(m => m.HomeModule) },
+  { path: 'generateqr', loadChildren: () => import('./screens/generateqr/generateqr.module').then(m => m.GenerateqrModule) },
   { path: 'movements', loadChildren: () => import('./screens/movements/movements.module').then(m => m.MovementsModule) },
   { path: 'temperature', loadChildren: () => import('./screens/temperature/temperature.module').then(m => m.TemperatureModule) },
   { path: 'kits', loadChildren: () => import('./screens/kits/kits.module').then(m => m.KitsModule) }
--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,125 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { SitesService } from './services/sites/sites.service';
-import { VenuesService } from './services/venues/venues.service';
-import { OfficeService } from './services/office/office.service';
-import { formatDate } from '@angular/common';
-import Swal from 'sweetalert2';
-
-const TOAST = Swal.mixin({
-  toast: true,
-  position: 'top-end',
-  showConfirmButton: false,
-  timer: 1500,
-  showCloseButton: true,
-  onOpen: (TOAST) => {
-    TOAST.addEventListener('mouseenter', Swal.stopTimer)
-    TOAST.addEventListener('mouseleave', Swal.resumeTimer)
-  }
-});
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-
 export class HomeComponent implements OnInit {
 
-  disabled: boolean = true
-  loading: boolean = false
-  showQrInfo: boolean = false;
+  constructor() { }
 
-  selectedVenue: string = '';
-  selectedOffice: string = '';
-  selectedSite: string = '';
-
-  qrHormiguero: string = '';
-  qrHormigueroS: string = '';
-  qrKit: string = '';
-  myDate: any;
-
-  venues: any[] = [];
-  offices: any[] = [];
-  sites: any[] = [];
-
-  resultVenues: any[] = [];
-  resultOffices: any[] = [];
-  resultSites: any[] = [];
-  resultNameSites: any[] = [];
-
-  idSites: any;
-
-  constructor(
-    private _venuesService: VenuesService,
-    private _officeService: OfficeService,
-    private _sitesService: SitesService,
-  ) {}
-
-  ngOnInit() {
-    this.getVenues();
+  ngOnInit(): void {
   }
 
-  onSubmit() {
-    this.showQrInfo = true;
-    this.loading = false;
-    this.resultNameSites = this.sites
-      .find(sites => sites['_id'] == this.idSites).nombre;
-    this.qrHormiguero = `${this.idSites}` + ':entrada'
-    this.qrHormigueroS = `${this.idSites}` + ':salida'
-    // console.log(this.resultNameSites);
-    // console.log(this.qrHormiguero);
-    // console.log(this.qrHormigueroS);
+  scroll(){
+    document.getElementById('cards')?.scrollIntoView({behavior:'smooth'});
   }
 
-  getVenues() {
-    this._venuesService.getVenueList().subscribe((data: any) => {
-      this.venues = data;
-      this.resultVenues = this.venues
-        .map(
-          (venues) => (venues['_id'])
-        );
-      // console.log(this.resultVenues);
-    });
-  }
+  public cards: any[] = [
+    {
+      img: '../../../assets/images/Logo-seti.png',
+      tittle: 'Sitios Seti',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, consequuntur assumenda'
+    },
+    {
+      img: '../../../assets/images/Logo-seti.png',
+      tittle: 'Perfilamiento',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, consequuntur assumenda'
+    }
+  ];
 
-  onChangeVenue(value: string) {
-    // console.log(value);
-    this._officeService.getOfficeByVenueId(value).subscribe(
-      (data: any) => {
-        this.offices = data;
-
-        this.resultOffices = this.offices
-          .map(
-            (offices) => (offices['_id'])
-          );
-        // console.log(this.resultOffices);
-      }
-    )
-  }
-
-  onChangeOffice(value: any) {
-    // console.log(value);
-    this._sitesService.getSitesByOfficeId(value).subscribe(
-      (data: any) => {
-        this.sites = data;
-
-        this.resultSites = this.sites
-          .map(
-            (sites) => (sites['_id'])
-          );
-        // console.log(this.resultSites);
-      }
-    )
-  }
-
-  onChangeSite(value: any) {
-    this.idSites = value;
-  }
-
-  onSubmitKit() {
-    this.myDate = formatDate(new Date(), 'yyyy-MM-dd-h:mm-a', 'en')
-    this.qrKit = `${this.myDate}` + ':kit';
-    // console.log(this.qrKit);
-  }
 
 }
