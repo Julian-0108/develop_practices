36646d8e0e1c01ba3f993044f64461ba6ce22373
hanna446 <hannamaria446@gmail.com>
2020-12-30 14:49:58 -0500

    development:profile options section is being created

--- src/app/screens/profile-options/profile-options.component.ts
+++ src/app/screens/profile-options/profile-options.component.ts
@@ -1,20 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-profile-options',
-  templateUrl: './profile-options.component.html',
-  styleUrls: ['./profile-options.component.scss']
+  selector: "app-profile-options",
+  templateUrl: "./profile-options.component.html",
+  styleUrls: ["./profile-options.component.scss"],
 })
 export class ProfileOptionsComponent implements OnInit {
-  title = 'Equipos Base'
-  description = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.Maxime, consequuntur assumenda'
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  x() {
-    console.log('hola')
-  }
+  title = "Equipos Base";
+  x = false;
+  description =
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Maxime, consequuntur assumenda";
+  items = [
+    {
+      title: "Desarrollo de Operaciones",
+      content: [
+        { profile: "Profesional en Formación", leves: [] },
+        { profile: "Consultor Junior", leves: ['N1','N2','N3'] },
+        { profile: "Consultor Especialista", leves: ['N1','N2','N3'] },
+        { profile: "Consultor Senior", leves: ['N1','N2','N3'] },
+      ],
+      image: "../../../assets/images/Grupo 756.svg",
+    },
+    {
+      title: "Desarrollo de Producto",
+      content: [
+        { profile: "Profesional en Formación", leves: [] },
+        { profile: "Consultor Junior", leves: ['N1','N2','N3'] },
+        { profile: "Consultor Especialista", leves: ['N1','N2','N3'] },
+        { profile: "Consultor Senior", leves: ['N1','N2','N3'] },
+      ],
+      image: "../../../assets/images/Grupo 757.svg",
+    },
+    {
+      title: "Bases de Datos",
+      content: [
+        { profile: "Profesional en Formación", leves: [] },
+        { profile: "Consultor Junior", leves: ['N1','N2','N3'] },
+        { profile: "Consultor Especialista", leves: ['N1','N2','N3'] },
+        { profile: "Consultor Senior", leves: ['N1','N2','N3'] },
+      ],
+      image: "../../../assets/images/Grupo 755.svg",
+    },
+  ];
+  constructor() {}
+  ngOnInit(): void {}
 
 }
