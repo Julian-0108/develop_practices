817887d811ffcd7f8e26c58c64fbebdcbf7de25d
Santiago Patiño <santipmartinez@outlook.com>
2020-12-30 09:30:21 -0500

    feat: agregado url en variables de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  API_MUNDO_SETI: 'http://192.168.27.27:6083/api/v1/masterinfo'
 };
 
 export const API_URL = 'https://sitios.setiaws.com';
