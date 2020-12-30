09190e3e24895541419cad14383080944faaf55d
Santiago Patiño <santipmartinez@outlook.com>
2020-12-30 16:29:24 -0500

    refactor: cambio de vistas a determinada categoria

--- src/app/screens/sites/temperature/temperature.module.ts
+++ src/app/screens/sites/temperature/temperature.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { TemperatureRoutingModule } from './temperature-routing.module';
 import { TemperatureComponent } from './temperature.component';
-import { SharedModule } from '../../shared/shared.module';
+import { SharedModule } from '@shared/shared.module';
 @NgModule({
   declarations: [TemperatureComponent],
   imports: [
