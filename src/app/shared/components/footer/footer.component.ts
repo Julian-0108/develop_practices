3c70f298b1558c55bb6708ea1812c495d1bb75f0
Wilmer Esteban Contreras Garcia <wilmercontreras@SETIAD.COM.CO>
2020-12-31 13:27:30 -0500

    Feat: footer component styles was updated

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  date: Date = new Date();
+  
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
