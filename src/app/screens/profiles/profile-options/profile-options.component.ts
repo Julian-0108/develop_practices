bdc4fdd9cae39c8ee6b26a0aadcce71620310c4b
hanna446 <hannamaria446@gmail.com>
2021-01-04 08:24:19 -0500

    development: notification component is being created and show levels option from profile option component is being modificated

--- src/app/screens/profiles/profile-options/profile-options.component.ts
+++ src/app/screens/profiles/profile-options/profile-options.component.ts
@@ -1,53 +1,69 @@
-import { Component, OnInit, HostListener, Directive } from "@angular/core";
+import { Component, OnInit, HostListener, Directive } from '@angular/core';
+import { NotificationService } from '@app/shared/components/notification/services/notification.service';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
-  selector: "app-profile-options",
-  templateUrl: "./profile-options.component.html",
-  styleUrls: ["./profile-options.component.scss"],
+  selector: 'app-profile-options',
+  templateUrl: './profile-options.component.html',
+  styleUrls: ['./profile-options.component.scss'],
 })
 export class ProfileOptionsComponent implements OnInit {
-  title = "Equipos Base";
-  x = false;
+  title = 'Equipos Base';
+  x;
+  cardClicked = '';
   description =
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Maxime, consequuntur assumenda";
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.Maxime, consequuntur assumenda';
   items = [
     {
-      title: "Desarrollo de Operaciones",
+      id: 'card1',
+      title: 'Desarrollo de Operaciones',
       content: [
-        { profile: "Profesional en Formación", levels: [], id:'btn1'},
-        { profile: "Consultor Junior", levels: ['N1','N2','N3'], id:'btn2'},
-        { profile: "Consultor Especialista", levels: ['N1'], id:'btn3'},
-        { profile: "Consultor Senior", levels: ['N1','N2'], id:'btn4'},
+        { profile: 'Profesional en Formación', levels: [], id: 'btn1' },
+        { profile: 'Consultor Junior', levels: ['N1', 'N2', 'N3'], id: 'btn2' },
+        { profile: 'Consultor Especialista', levels: ['N1'], id: 'btn3' },
+        { profile: 'Consultor Senior', levels: ['N1', 'N2'], id: 'btn4' },
       ],
-      image: "../../../assets/images/Grupo 756.svg",
+      image: '../../../assets/images/Grupo 756.svg',
     },
     {
-      title: "Desarrollo de Producto",
+      id: 'card2',
+      title: 'Desarrollo de Producto',
       content: [
-        { profile: "Profesional en Formación", levels: [], id:'btn5'},
-        { profile: "Consultor Junior", levels: ['N1','N2','N3'], id:'btn6'},
-        { profile: "Consultor Especialista", levels: ['N1'], id:'btn7'},
-        { profile: "Consultor Senior", levels: ['N1','N2'], id:'btn8'},
+        { profile: 'Profesional en Formación', levels: [], id: 'btn5' },
+        { profile: 'Consultor Junior', levels: ['N1', 'N2', 'N3'], id: 'btn6' },
+        { profile: 'Consultor Especialista', levels: ['N1'], id: 'btn7' },
+        { profile: 'Consultor Senior', levels: ['N1', 'N2'], id: 'btn8' },
       ],
-      image: "../../../assets/images/Grupo 757.svg",
+      image: '../../../assets/images/Grupo 757.svg',
     },
     {
-      title: "Bases de Datos",
+      id: 'card3',
+      title: 'Bases de Datos',
       content: [
-        { profile: "Profesional en Formación", levels: [], id:'btn9'},
-        { profile: "Consultor Junior", levels: ['N1','N2','N3'], id:'btn10'},
-        { profile: "Consultor Especialista", levels: ['N1'], id:'btn11'},
-        { profile: "Consultor Senior", levels: ['N1','N2'], id:'btn12'},
+        { profile: 'Profesional en Formación', levels: [], id: 'btn9' },
+        { profile: 'Consultor Junior', levels: ['N1', 'N2', 'N3'], id: 'btn10' },
+        { profile: 'Consultor Especialista', levels: ['N1'], id: 'btn11' },
+        { profile: 'Consultor Senior', levels: ['N1', 'N2'], id: 'btn12' },
       ],
-      image: "../../../assets/images/Grupo 755.svg",
+      image: '../../../assets/images/Grupo 755.svg',
     },
   ];
-  constructor() {}
+  constructor(private notificationService: NotificationService) {
+    this.x = '';
+  }
   ngOnInit(): void {}
 
-  y(element: any){
-    console.log(element);
-
+  ejemplo() {
+    const option = {
+      message: 'ejemplo',
+      action: 'aceptar',
+    };
+    this.notificationService
+      .openSnackBar(option)
+      .afterDismissed()
+      .subscribe(() => {
+        console.log('hola');
+      });
   }
 
 }
--- src/app/shared/components/notification/services/notification.service.ts
+++ src/app/shared/components/notification/services/notification.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
+interface snackOptionsInterface {
+  message: string;
+  action?: string;
+  horizontalPosition?: MatSnackBarHorizontalPosition;
+  verticalPosition?: MatSnackBarVerticalPosition;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationService {
+  constructor(private _snackBar: MatSnackBar) {}
 
-  constructor() { }
+  openSnackBar(options: snackOptionsInterface) {
+    return this._snackBar.open(options.message, options.action || '', {
+      duration: 5000,
+      horizontalPosition: options.horizontalPosition || 'right',
+      verticalPosition: options.verticalPosition || 'top',
+    });
+  }
 }
