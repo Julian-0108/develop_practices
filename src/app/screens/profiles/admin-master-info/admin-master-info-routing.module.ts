8dce350d23a6e3dead2b69bed242660766222796
Santiago Patiño <santipmartinez@outlook.com>
2021-01-28 14:28:38 -0500

    feat: agregado guard

--- src/app/screens/profiles/admin-master-info/admin-master-info-routing.module.ts
+++ src/app/screens/profiles/admin-master-info/admin-master-info-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '@app/core/guards/auth.guard';
 import { AdminMasterInfoComponent } from './admin-master-info.component';
 
 
-const routes: Routes = [{ path: '', component: AdminMasterInfoComponent }];
+const routes: Routes = [{ path: '', component: AdminMasterInfoComponent, canActivate: [AuthGuard] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
