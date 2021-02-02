4e30d7f2d27b3a0bcaaf10f173b2c80e69714a9f
hanna446 <hannamaria446@gmail.com>
2021-02-02 11:16:59 -0500

    development: merge profile screen and history screen

---
+++ src/app/screens/profiles/profile-template/profile-form-history/profile-form-history.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import moment from 'moment';
+
+@Component({
+  selector: 'app-profile-form-history',
+  templateUrl: './profile-form-history.component.html',
+  styleUrls: ['./profile-form-history.component.scss']
+})
+export class ProfileFormHistoryComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<ProfileFormHistoryComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+
+  formHistory = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    date: new FormControl({value: '', disabled: true}),
+    description: new FormControl('', [Validators.required]),
+  });
+
+  ngOnInit(): void {
+    console.log(this.data)
+    this.formHistory.get('date')?.patchValue(moment(new Date()).format('YYYY-MM-DD'));
+  }
+
+  onSave() {
+    this.dialogRef.close({...this.formHistory.value, idProfile: this.data.profileDate._id, date: this.formHistory.value.date});
+  }
+
+
+}
