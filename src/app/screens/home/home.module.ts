327a31a26c8e0177cca265efd9735327562c073b
Santiago Patiño <santipmartinez@outlook.com>
2021-02-02 14:10:56 -0500

    eliminado servicio de providers

--- src/app/screens/home/home.module.ts
+++ src/app/screens/home/home.module.ts
@@ -1,13 +1,11 @@
 //angular imports
 import { NgModule } from '@angular/core';
 
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 
 // Custom Imports
-import { QRCodeModule } from 'angularx-qrcode';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HomeService } from './services/home.service';
 
@@ -15,10 +13,8 @@ import { HomeService } from './services/home.service';
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    HomeRoutingModule,
     SharedModule,
-    QRCodeModule
-  ],
-  providers: [HomeService]
+    HomeRoutingModule
+  ]
 })
 export class HomeModule { }
