231b0f8f8e37c2576164c4a21c0b936f39fd062c
Santiago Patiño <santipmartinez@outlook.com>
2021-02-02 11:04:41 -0500

    feat: agregado api de inicio de sesion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  API_MUNDO_SETI: 'http://192.168.27.27:6083/api/v1/masterinfo'
+  API_MUNDO_SETI: 'http://192.168.27.27:6083/api/v1/masterinfo',
+  API_SECURITY: 'http://192.168.27.27:6083/api/v1/security'
 };
 
 export const API_URL = 'https://sitios.setiaws.com';
