dae3c990f5d327f2445a0567e7760d7c966e87ca
Santiago Patiño <santipmartinez@outlook.com>
2021-02-03 14:46:44 -0500

    feat: added custom paginator

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -21,64 +21,66 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatTableModule } from '@angular/material/table';
 import { FlexModule } from '@angular/flex-layout';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatRadioModule } from '@angular/material/radio';
-import {MatStepperModule} from '@angular/material/stepper';
+import { MatStepperModule } from '@angular/material/stepper';
 
-import {MatChipsModule} from '@angular/material/chips';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
-
-
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { CustomPaginator } from './custom-paginator';
 
 const materialComponents = [
-    MatPaginatorModule,
-    MatSortModule,
-    MatSlideToggleModule,
-    MatTooltipModule,
-    MatIconModule,
-    MatButtonModule,
-    MatProgressSpinnerModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatRippleModule,
-    MatSnackBarModule,
-    FlexModule,
-    FlexLayoutModule,
-    MatSelectModule,
-    MatCardModule,
-    MatGridListModule,
-    MatFormFieldModule,
-    MatMenuModule,
-    MatExpansionModule,
-    MatDividerModule,
-    MatListModule,
-    MatAutocompleteModule,
-    MatSliderModule,
-    MatTableModule,
-    ReactiveFormsModule,
-    MatButtonToggleModule,
-    MatDialogModule,
-    MatRadioModule,
-    MatStepperModule,
-    MatChipsModule,
-    MatTabsModule,
-    MatDatepickerModule,
-    MatNativeDateModule
+  MatPaginatorModule,
+  MatSortModule,
+  MatSlideToggleModule,
+  MatTooltipModule,
+  MatIconModule,
+  MatButtonModule,
+  MatProgressSpinnerModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatRippleModule,
+  MatSnackBarModule,
+  FlexModule,
+  FlexLayoutModule,
+  MatSelectModule,
+  MatCardModule,
+  MatGridListModule,
+  MatFormFieldModule,
+  MatMenuModule,
+  MatExpansionModule,
+  MatDividerModule,
+  MatListModule,
+  MatAutocompleteModule,
+  MatSliderModule,
+  MatTableModule,
+  ReactiveFormsModule,
+  MatButtonToggleModule,
+  MatDialogModule,
+  MatRadioModule,
+  MatStepperModule,
+  MatChipsModule,
+  MatTabsModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
 ];
 
 @NgModule({
-    imports: [materialComponents],
-    exports: [materialComponents],
-    providers: [ {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 2500 } }]
-  })
-  export class MaterialModule {}
+  imports: [materialComponents],
+  exports: [materialComponents],
+  providers: [
+    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 2500 } },
+    { provide: MatPaginatorIntl, useClass: CustomPaginator },
+  ],
+})
+export class MaterialModule {}
