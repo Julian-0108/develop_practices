f87bb96a9e80f02ad6c536f4d3b192d4d4a0c6eb
Santiago Patiño <santipmartinez@outlook.com>
2021-02-04 08:01:00 -0500

    feat: mensaje de error

--- src/app/screens/profiles/adminprofiles/adminprofiles.component.ts
+++ src/app/screens/profiles/adminprofiles/adminprofiles.component.ts
@@ -3,6 +3,7 @@ import { Title } from '@angular/platform-browser';
 import { ManageProfileService } from './services/manage-profile.service';
 import { Master } from '@shared/interfaces/master.interface';
 import { environment } from '@environments/environment';
+import { NotificationService } from '../../../shared/components/notification/services/notification.service';
 
 @Component({
   selector: 'app-adminprofiles',
@@ -14,7 +15,8 @@ export class AdminprofilesComponent implements OnInit {
   public API_MUNDO_SETI: string = environment.API_MUNDO_SETI;
   constructor(
     private titleService: Title,
-    private manageProfileService: ManageProfileService
+    private manageProfileService: ManageProfileService,
+    private notificationService: NotificationService
   ) {
     this.titleService.setTitle('Mundo SETI - administrar perfiles');
   }
@@ -26,7 +28,7 @@ export class AdminprofilesComponent implements OnInit {
   getData(): void {
     this.manageProfileService.getData()
     .then( response => this.cards = response)
-    .catch( err => console.log('error'));
+    .catch( err => this.notificationService.openSimpleSnackBar({ title: 'Error', type: 'error', message: 'Error cargando el menú. Vuelve a intentar'}));
   }
 
 }
