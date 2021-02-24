076d536845203b34db1fe47cd6a15a93dc699318
Santiago Patiño <santipmartinez@outlook.com>
2021-02-24 09:15:32 -0500

    refactor: actualizado can antivate en el routing principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '@core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
@@ -10,20 +11,24 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./screens/home/home.module').then((m) => m.HomeModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'movements',
     loadChildren: () =>
       import('./screens/sites/movements/movements.module').then((m) => m.MovementsModule),
+      canActivate: [AuthGuard]
   },
   {
     path: 'temperature',
     loadChildren: () =>
       import('./screens/sites/temperature/temperature.module').then((m) => m.TemperatureModule),
+      canActivate: [AuthGuard]
   },
   {
     path: 'kits',
     loadChildren: () => import('./screens/sites/kits/kits.module').then((m) => m.KitsModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'profile-options',
@@ -31,6 +36,7 @@ const routes: Routes = [
       import('./screens/profiles/profile-options/profile-options.module').then(
         (m) => m.ProfileOptionsModule
       ),
+      canActivate: [AuthGuard]
   },
   {
     path: 'profile-template/:idProfile',
@@ -38,11 +44,13 @@ const routes: Routes = [
       import('./screens/profiles/profile-template/profile-template.module').then(
         (m) => m.ProfileTemplateModule
       ),
+      canActivate: [AuthGuard]
   },
   {
     path: 'generateqr',
     loadChildren: () =>
       import('./screens/sites/generateqr/generateqr.module').then((m) => m.GenerateqrModule),
+      canActivate: [AuthGuard]
   },
   {
     path: 'admin-profiles',
@@ -50,6 +58,7 @@ const routes: Routes = [
       import('./screens/profiles/adminprofiles/adminprofiles.module').then(
         (m) => m.AdminprofilesModule
       ),
+      canActivate: [AuthGuard]
   },
   {
     path: 'master-info',
@@ -57,6 +66,7 @@ const routes: Routes = [
       import('./screens/profiles/admin-master-info/admin-master-info.module').then(
         (m) => m.AdminMasterInfoModule
       ),
+      canActivate: [AuthGuard]
   },
   {
     path: 'admin-profiles',
@@ -64,13 +74,7 @@ const routes: Routes = [
       import('./screens/profiles/adminprofiles/adminprofiles.module').then(
         (m) => m.AdminprofilesModule
       ),
-  },
-  {
-    path: 'master-info',
-    loadChildren: () =>
-      import('./screens/profiles/admin-master-info/admin-master-info.module').then(
-        (m) => m.AdminMasterInfoModule
-      ),
+      canActivate: [AuthGuard]
   },
   {
     path: 'manage-base-teams/:id',
@@ -78,14 +82,19 @@ const routes: Routes = [
       import('./screens/profiles/manage-base-teams/manage-base-teams.module').then(
         (m) => m.ManageBaseTeamsModule
       ),
+      canActivate: [AuthGuard]
   },
   {
     path: 'error',
     loadChildren: () => import('./screens/error/error.module').then((m) => m.ErrorModule),
   },
   {
+    path: 'notPermits',
+    loadChildren: () => import('./screens/error/error.module').then((m) => m.ErrorModule),
+  },
+  {
     path: '**',
-    loadChildren: () => import('./screens/notfound/notfound.module').then((m) => m.NotfoundModule),
+    loadChildren: () => import('./screens/error/error.module').then((m) => m.ErrorModule),
   },
 ];
 
--- src/app/screens/profiles/adminprofiles/adminprofiles-routing.module.ts
+++ src/app/screens/profiles/adminprofiles/adminprofiles-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from '@app/core/guards/auth.guard';
 import { AdminprofilesComponent } from './adminprofiles.component';
 
 
-const routes: Routes = [{ path: '', component: AdminprofilesComponent, canActivate: [AuthGuard] }];
+const routes: Routes = [{ path: '', component: AdminprofilesComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
