c9a8f9cb0fdc50d9fec0b26dab1bcaef0117943c
hanna446 <hannamaria446@gmail.com>
2021-03-09 16:54:03 -0500

    :sparkles: | The cards have been sorted in the position requested

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HomeService } from './services/home.service';
-import { environment } from "@environments/environment";
-
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  @ViewChild('cardsHTML') cardsHTML : ElementRef | undefined;
+  @ViewChild('cardsHTML') cardsHTML: ElementRef | undefined;
   public cards: any;
   public API_MASTER_INFO = environment.API_MASTER_INFO;
+  // sortOrder: any = [];
+  sortOrder: any = ['Sitios SETI', 'Perfilamiento', 'Universidad SETI', 'Administrar Recursos'];
 
   constructor(private homeService: HomeService) {}
 
@@ -21,16 +21,18 @@ export class HomeComponent implements OnInit {
   }
 
   getModules(): void {
-    this.homeService.getModules()
-    .then( res => {
-      this.cards = res;
-    })
-    .catch(err => console.log(err))
+    this.homeService
+      .getModules()
+      .then((res) => {
+        res.sort((a: any, b: any) => {
+          return this.sortOrder.indexOf(a.name) - this.sortOrder.indexOf(b.name);
+        });
+        this.cards = res;
+      })
+      .catch((err) => console.log(err));
   }
 
-  scroll(){
-    this.cardsHTML?.nativeElement.scrollIntoView({behavior:'smooth'});
+  scroll() {
+    this.cardsHTML?.nativeElement.scrollIntoView({ behavior: 'smooth' });
   }
-
-
 }
