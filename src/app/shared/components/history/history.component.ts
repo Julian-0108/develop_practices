1ec3a38ffae3bd0cc8841233a148a882932d055e
hanna446 <hannamaria446@gmail.com>
2021-03-15 17:28:49 -0500

    :sparkles: | working in previsualize and filter functionalities

--- src/app/shared/components/history/history.component.ts
+++ src/app/shared/components/history/history.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import * as dayjs from 'dayjs';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import isBetween from 'dayjs/plugin/isBetween';
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isBetween);
 
 @Component({
   selector: 'app-history',
@@ -9,29 +16,89 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class HistoryComponent implements OnInit {
   constructor() {}
   displayedColumns: string[] = ['name', 'levels', 'status'];
+  historySelected = '';
+  visivility = false;
+  dataSource = [];
   historyFilter: any = [
     {
       showDate: 'ejemplo',
       author: 'ejemplo',
       descriptionChanges: 'ejemplo',
-      createdAt: '10/03/2021',
-      _id: 'fjnufufUubybui',
+      updatedAt: '2021-03-10',
+      _id: 'id1',
+      name: 'ejemplo',
+      levels: ['N1', 'N2', 'N3'],
+      status: true,
     },
-  ];
-  dataSource: any = [
     {
-      name: 'Analista Especialista',
-      levels: ['N1','N2','N3'],
+      showDate: 'ejemplo2',
+      author: 'ejemplo2',
+      descriptionChanges: 'ejemplo2',
+      updatedAt: '2021-03-05',
+      _id: 'id2',
+      name: 'ejemplo2',
+      levels: ['N1', 'N2', 'N3'],
+      status: true,
+    },
+    {
+      showDate: 'ejemplo3',
+      author: 'ejemplo3',
+      descriptionChanges: 'ejemplo3',
+      updatedAt: '2021-03-11',
+      _id: 'id3',
+      name: 'ejemplo3',
+      levels: ['N1', 'N2', 'N3'],
       status: true,
     },
   ];
+
   formFilterHistory = new FormGroup({
     startDate: new FormControl(),
     endDate: new FormControl(),
   });
 
-  ngOnInit(): void {}
-  onSelectStartDate(event: any) {}
-  onSelectEndDate(event: any) {}
-  onPreview(id: any) {}
+  ngOnInit(): void {
+    this.getHistory();
+  }
+  getHistory() {
+    // this.historySelected = this.historyFilter[0]._id;
+  }
+  /**
+   * @author Hanna
+   * @description Función que filtra el contenido del historial con la
+   * fecha inicial digitada y como fecha final, la fecha actual por defecto.
+   */
+  onSelectStartDate(event: any) {
+    const startDate = dayjs.default(event.value).format('YYYY-MM-DD');
+    const endDate =
+      this.formFilterHistory.value.endDate === null
+        ? dayjs.default(new Date()).format('YYYY-MM-DD')
+        : dayjs.default(this.formFilterHistory.value.endDate).format('YYYY-MM-DD');
+
+    this.historyFilter = this.historyFilter.filter((item: any) =>
+      dayjs.default(item.updatedAt).isBetween(startDate, endDate)
+    );
+  }
+
+  /**
+   * @author Hanna
+   * @description Función que filtra el contenido del historial con la
+   * fecha inicial digitada y la fecha final digitada.
+   */
+  onSelectEndDate(event: any) {
+    const endDate = dayjs.default(event.value).format('YYYY-MM-DD');
+    const startDate = dayjs.default(this.formFilterHistory.value.startDate).format('YYYY-MM-DD');
+    this.historyFilter = this.historyFilter.filter((item: any) =>
+      dayjs.default(item.updatedAt).isBetween(startDate, endDate)
+    );
+  }
+  onPreview(id: any) {
+    this.historySelected = id;
+    this.visivility = true;
+    this.dataSource = this.historyFilter.filter((el: any) => el._id === id);
+  }
+  outPreview() {
+    this.historySelected = '';
+    this.visivility = false;
+  }
 }
