964b80f907e99aa377d35d3c5ed2a61fa04db4e7
Wilmer Esteban Contreras Garcia <wilmercontreras@SETIAD.COM.CO>
2021-04-07 17:25:47 -0500

    :art: fix some corrections

--- src/app/screens/profiles/profile-template/profile-template.component.ts
+++ src/app/screens/profiles/profile-template/profile-template.component.ts
@@ -275,6 +275,9 @@ export class ProfileTemplateComponent implements OnInit {
         if (this.tabIndexTalents === length - 1) {
           this.nextPageButtonDisabledTalents = true;
         }
+        // if (length < 12) {
+        //   this.nextPageButtonDisabledTalents = true;
+        // }
         break;
       case 'talentsReadOnly':
         this.tabIndexTalentsReadOnly = this.tabIndexTalentsReadOnly + 1;
@@ -684,7 +687,7 @@ export class ProfileTemplateComponent implements OnInit {
     if (this.education.selectedOptions.selected.length === 0) {
       this.notificationService.openSimpleSnackBar({
         title: 'Acción Incorrecta',
-        message: 'Debe seccionar al menos un item de la lista de "Formación Académica".',
+        message: 'Debe seleccionar al menos un item de la lista de "Formación Académica".',
         type: 'error',
       });
       this.educationError = true;
@@ -701,7 +704,7 @@ export class ProfileTemplateComponent implements OnInit {
     if (this.requiredCertificates.selectedOptions.selected.length === 0) {
       this.notificationService.openSimpleSnackBar({
         title: 'Acción Incorrecta',
-        message: 'Debe seccionar al menos un item de la lista de "Certificaciones Requeridas". ',
+        message: 'Debe seleccionar al menos un item de la lista de "Certificaciones Requeridas". ',
         type: 'error',
       });
       this.requiredCertificatesError = true;
@@ -720,7 +723,7 @@ export class ProfileTemplateComponent implements OnInit {
     if (this.specificKnowledge.selectedOptions.selected.length === 0) {
       this.notificationService.openSimpleSnackBar({
         title: 'Acción Incorrecta',
-        message: 'Debe seccionar al menos un item de la lista de "Conocimientos Específicos".',
+        message: 'Debe seleccionar al menos un item de la lista de "Conocimientos Específicos".',
         type: 'error',
       });
       this.specificKnowledgeError = true;
@@ -739,7 +742,7 @@ export class ProfileTemplateComponent implements OnInit {
     if (this.rolResponsabilities.selectedOptions.selected.length === 0) {
       this.notificationService.openSimpleSnackBar({
         title: 'Acción Incorrecta',
-        message: 'Debe seccionar al menos un item de la lista de "Funciones del Rol".',
+        message: 'Debe seleccionar al menos un item de la lista de "Funciones del Rol".',
         type: 'error',
       });
       this.rolResponsabilitiesError = true;
@@ -758,7 +761,7 @@ export class ProfileTemplateComponent implements OnInit {
     if (this.talents.selectedOptions.selected.length === 0) {
       this.notificationService.openSimpleSnackBar({
         title: 'Acción Incorrecta',
-        message: 'Debe seccionar al menos un item de la lista de "Talentos".',
+        message: 'Debe seleccionar al menos un item de la lista de "Talentos".',
         type: 'error',
       });
       this.talentsError = true;
@@ -776,7 +779,7 @@ export class ProfileTemplateComponent implements OnInit {
       this.notificationService.openSimpleSnackBar({
         title: 'Acción Incorrecta',
         message:
-          'Debe seccionar al menos un item de la lista de "Responsabilidades para Seguridad y Salud en el Trabajo".',
+          'Debe seleccionar al menos un item de la lista de "Responsabilidades para Seguridad y Salud en el Trabajo".',
         type: 'error',
       });
       this.securityRespError = true;
