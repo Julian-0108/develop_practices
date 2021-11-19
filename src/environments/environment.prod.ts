35bfbb5c2b46fdde122080fc4b96e15182242d01
Juan Sebastian LLano Taborda <juanllano@SETIAD.COM.CO>
2021-11-19 11:27:34 -0500

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   API_MASTER_INFO:  `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/v1/masterinfo`,
   API_BASE_PROFILES : `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/v1/baseprofiles`,
-  API_SECURITY: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/v1/security`
+  API_SECURITY: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/v1/security-admin`
 };
 
 export const API_URL = 'https://sitios.setiaws.com';
