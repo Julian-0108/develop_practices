1f51fa1543611713dbb48fa6227ba80c9453aaf7
Andres Duque Palacio <andresduque@seti.com.co>
2021-12-28 18:02:31 -0500

    :construction: Creacion nuevo componente profile-manage-resumes

---
+++ src/app/screens/profiles/profile-manage-resumes/profile-manage-resumes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-manage-resumes',
+  templateUrl: './profile-manage-resumes.component.html',
+  styleUrls: ['./profile-manage-resumes.component.scss']
+})
+export class ProfileManageResumesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/screens/profiles/profile-manage-resumes/profile-manage-resumes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProfileManageResumesRoutingModule } from './profile-manage-resumes-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProfileManageResumesRoutingModule
+  ]
+})
+export class ProfileManageResumesModule { }
