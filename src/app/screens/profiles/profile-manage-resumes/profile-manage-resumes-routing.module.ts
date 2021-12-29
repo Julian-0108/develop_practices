5d8aced2099cc533e20be7e5a1a0dcf75fc27850
Andres Duque Palacio <andresduque@seti.com.co>
2021-12-29 15:35:32 -0500

    :truck: agregar ruta componente profile-manage-resumes

--- src/app/screens/profiles/profile-manage-resumes/profile-manage-resumes-routing.module.ts
+++ src/app/screens/profiles/profile-manage-resumes/profile-manage-resumes-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileManageResumesComponent } from './profile-manage-resumes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: ProfileManageResumesComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
