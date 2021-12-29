d485f3e906c2f45cbe66376ea7dc804b0e28bd3d
Andres Duque Palacio <andresduque@seti.com.co>
2021-12-29 15:49:55 -0500

    :rocket: Agregar infomacion data carta Agregar Hv

--- src/app/screens/profiles/profile-manage-resumes/profile-manage-resumes.component.ts
+++ src/app/screens/profiles/profile-manage-resumes/profile-manage-resumes.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'app-profile-manage-resumes',
   templateUrl: './profile-manage-resumes.component.html',
-  styleUrls: ['./profile-manage-resumes.component.scss']
+  styleUrls: ['./profile-manage-resumes.component.scss'],
 })
 export class ProfileManageResumesComponent implements OnInit {
+  title = 'Gestion HV';
 
-  constructor() { }
+  public cards: any[] = [
+    {
+      name: 'Agregar HV',
+      description: 'Inserción de nuevas hojas de vida',
+      url: '/underConstruction',
+      imagePath: '',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  public API_MASTER_INFO: string = environment.API_MASTER_INFO;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
