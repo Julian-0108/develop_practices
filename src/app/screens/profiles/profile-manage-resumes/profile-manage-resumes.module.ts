34e68865810e0a06abc5887adeb77c82f9386e1e
Andres Duque Palacio <andresduque@seti.com.co>
2021-12-29 15:44:05 -0500

    :pushpin: agregar y asignar importaciones de shared y componente

--- src/app/screens/profiles/profile-manage-resumes/profile-manage-resumes.module.ts
+++ src/app/screens/profiles/profile-manage-resumes/profile-manage-resumes.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProfileManageResumesRoutingModule } from './profile-manage-resumes-routing.module';
-
+import { ProfileManageResumesComponent } from './profile-manage-resumes.component';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileManageResumesComponent
+  ],
   imports: [
     CommonModule,
+    SharedModule,
     ProfileManageResumesRoutingModule
   ]
 })
