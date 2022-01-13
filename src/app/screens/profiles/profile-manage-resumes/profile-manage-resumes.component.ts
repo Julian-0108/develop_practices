2acf92c6d6ebea1d6d6efc7ad4df1ec23bd5a24a
Andres Duque Palacio <andresduque@seti.com.co>
2022-01-13 12:21:41 -0500

    a:truck: renombrar campos tabla de busqueda hv

--- src/app/screens/profiles/profile-manage-resumes/profile-manage-resumes.component.ts
+++ src/app/screens/profiles/profile-manage-resumes/profile-manage-resumes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '@environments/environment';
+import { MatTableDataSource } from '@angular/material/table';
+import { Search } from './interfaces/manege-resumes.interface';
+import { AddResumeComponent } from './add-resume/add-resume.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-profile-manage-resumes',
@@ -7,20 +10,38 @@ import { environment } from '@environments/environment';
   styleUrls: ['./profile-manage-resumes.component.scss'],
 })
 export class ProfileManageResumesComponent implements OnInit {
-  title = 'Gestion HV';
+  title = 'Gestión HV';
 
-  public cards: any[] = [
-    {
-      name: 'Agregar HV',
-      description: 'Inserción de nuevas hojas de vida',
-      url: '/underConstruction',
-      imagePath: '',
-    },
+  public displayedColumns: string[] = [
+    'name',
+    'documentNumber',
+    'tool',
+    'processStatus',
+    'wageAspiration',
   ];
 
-  public API_MASTER_INFO: string = environment.API_MASTER_INFO;
+  ELEMENT_DATA: Search[] = [
+    {
+      name: 'Andres Duque Palacio',
+      documentNumber: 1193125683,
+      tool: 'Angular',
+      processStatus: 'Seleccionado',
+      wageAspiration: 3000000,
+    }
+  ];
 
-  constructor() {}
+  dataSource = new MatTableDataSource<Search>(this.ELEMENT_DATA);
+
+  constructor(
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {}
+
+  openDialogAdd(){
+    const dialogRef = this.dialog.open(AddResumeComponent, {
+      width: '60%',
+      height: '80%'
+    });
+  }
 }
