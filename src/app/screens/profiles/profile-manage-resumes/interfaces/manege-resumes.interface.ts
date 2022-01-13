27f554b473c9dcffde4b740e9eb31f85189bc895
Andres Duque Palacio <andresduque@seti.com.co>
2022-01-13 12:13:22 -0500

    añadir Ãinterface tabla busqueda

---
+++ src/app/screens/profiles/profile-manage-resumes/interfaces/manege-resumes.interface.ts
@@ -0,0 +1,12 @@
+/**
+ * @autor Andres Duque Palacio
+ * @description Interfaz para componente manage resumes (Gestion HV)
+ */
+
+ export interface Search {
+    name: string;
+    documentNumber: number;
+    tool: string;
+    processStatus: string;
+    wageAspiration: number;
+  }
