23561224d269e55502f7d6de920c1fc85bd1bc13
Andres Duque Palacio <andresduque@seti.com.co>
2022-02-15 15:42:59 -0500

    añadir importacion

--- src/app/screens/profiles/profile-manage-resumes/profile-manage-resumes.module.ts
+++ src/app/screens/profiles/profile-manage-resumes/profile-manage-resumes.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ProfileManageResumesRoutingModule } from './profile-manage-resumes-routing.module';
 import { ProfileManageResumesComponent } from './profile-manage-resumes.component';
 import { SharedModule } from '@shared/shared.module';
+import { ToolsComponent } from './dialogs/tools/tools.component';
+import { SeeResumeComponent } from './dialogs/see-resume/see-resume.component';
+import { AssociateGLPIComponent } from './dialogs/associate-glpi/associate-glpi.component';
 
 @NgModule({
   declarations: [
-    ProfileManageResumesComponent
+    ProfileManageResumesComponent,
+    ToolsComponent,
+    SeeResumeComponent,
+    AssociateGLPIComponent
   ],
   imports: [
     CommonModule,
