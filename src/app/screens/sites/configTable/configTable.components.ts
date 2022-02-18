8373f33afd3a9e0e0e1a8121b79494bb5e3382c3
Erick Leonardo Ramirez Hernandez <erickramirez@SETIAD.COM.CO>
2022-02-18 16:57:24 -0500

    :construction: Desarrollo configuracion de tablas

--- src/app/screens/sites/configTable/configTable.components.ts
+++ src/app/screens/sites/configTable/configTable.components.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatTableDataSource } from '@angular/material/table';
+import { Search } from '@app/screens/profiles/profile-manage-resumes/interfaces/manege-resumes.interface';
+import { ConfigTableServices } from './services/configTable.services';
 
 
 @Component({
@@ -9,14 +11,17 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./configTable.components.scss'],
 })
 export class ConfigTableComponents implements OnInit {
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getSites();
+  }
+  constructor(private service:ConfigTableServices) {}
   masterInfoService: any;
   otherIcon!: boolean;
   open: boolean = false;
   help: string = 'help';
   idHistory!: string;
   subtitle: any = '';
-  dataSource =new MatTableDataSource(ELEMENT_DATA);
+  dataSource: Search[] = [];
 
   public displayedColumns: string[] = [
     'name',
@@ -30,21 +35,17 @@ export class ConfigTableComponents implements OnInit {
   ];
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  //   // this.dataSource.filter() = filterValue.trim().toLowerCase();
+  }
+
+  getSites(){
+    this.service.getDataSites()
+      .then(dataValue => {
+        if(dataValue.length > 0){
+          this.dataSource = dataValue;
+        }
+      }).catch(error => {
+        console.log('error',error);
+      })
   }
 }
-export interface PeriodicElement {
-  name: string,
-  direction : string
-  phone: number
-  city: string
-  creationDate: string
-  actualizationDate: string
-  status : boolean
-  actions: string
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {name:'Erick', direction:'Villeta Colombia', phone: 3106887373, city:'villeta', creationDate: '20-04-2022', actualizationDate: '10/10/2023', status:true, actions:'edit'},
-  {name:'Leo', direction:'Villeta Colombia', phone: 3106887373, city:'villeta', creationDate: '17-02-2022', actualizationDate: '10/10/2023', status:true, actions:'edit'}
-];
