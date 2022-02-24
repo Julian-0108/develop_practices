95b9c4edc77a3182bcc46ea1e34e1ec0b44dd273
Juan Manuel Caro Vargas <juancaro@SETIAD.COM.CO>
2022-02-24 17:12:34 -0500

    :construction: se agrego el subtitulo y se arreglo la ortografia de algunas palabras

--- src/app/screens/sites/configTable/configTable.components.ts
+++ src/app/screens/sites/configTable/configTable.components.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatButton } from '@angular/material/button';
 import { MatTableDataSource } from '@angular/material/table';
-import { Search } from '@app/screens/profiles/profile-manage-resumes/interfaces/manege-resumes.interface';
-import { url } from 'inspector';
 import { SitesComponent } from './dialogs/sites.component';
 import { Tables } from './interfaces/configTable.interface';
 import { ConfigTableServices } from './services/configTable.services';
@@ -16,15 +13,15 @@ import { MatDialog } from '@angular/material/dialog';
 export class ConfigTableComponents implements OnInit {
   ngOnInit(): void {}
   public informationSites: boolean;
-  public subtitle: any;
 
   constructor(private service: ConfigTableServices, private dialog: MatDialog) {
-    this.informationSites = false
+    this.informationSites = false;
   }
   otherIcon!: boolean;
   open: boolean = false;
   help: string = 'help';
   idHistory!: string;
+  subtitle: any = '';
   dataSource: MatTableDataSource<Tables | any> = new MatTableDataSource();
 
   public displayedColumns: string[] = [
@@ -38,14 +35,19 @@ export class ConfigTableComponents implements OnInit {
     'actions',
   ];
 
-  public readonly table = [
+  public table = [
     {
-      name: 'Configuración de sedes',
+      name: 'Sedes',
       url: 'venues',
       sumary: 'aquí puedes configurar las sedes de la empresa',
       haveTypeField: false,
     },
-    { name: 'Oficinas', url: 'Oficinas' },
+    {
+      name: 'Oficinas',
+      url: 'Offices',
+      sumary: 'aquí puedes configurar las oficinas',
+      haveTypeField: false,
+    },
   ];
   applyFilter(filterValue: any) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -58,27 +60,32 @@ export class ConfigTableComponents implements OnInit {
   }
 
   getList(url: string) {
+    this.table.forEach((tb: any) => {
+      if (tb.url == url) {
+        this.subtitle = tb.name;
+      }
+    });
     this.service
       .getListSites(url)
       .then((dataValue) => {
         if (dataValue.length > 0) {
           this.dataSource = new MatTableDataSource(dataValue);
-          this.informationSites= true;
-          this.subtitle = this.table.map((tb: any) =>tb.name)
+          this.informationSites = true;
         }
-      }).catch((error) => {
-        this.dataSource= new MatTableDataSource();
-        this.informationSites=false
+      })
+      .catch((error) => {
+        this.dataSource = new MatTableDataSource();
+        this.informationSites = false;
         console.log('error', error);
       });
-
   }
+
   openEdit(value: any) {
-    console.log(value)
+    console.log(value);
     this.dialog.open(SitesComponent, {
       width: '60%',
       height: '80%',
-      data: {dataSite:value}
-  });
+      data: { dataSite: value },
+    });
   }
 }
