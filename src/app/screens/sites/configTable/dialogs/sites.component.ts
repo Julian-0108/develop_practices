886c39c8653232d40c34216a6e6685237649695e
Juan Manuel Caro Vargas <juancaro@SETIAD.COM.CO>
2022-03-02 09:13:44 -0500

    :construction: se agrego la funcionalidad del boton de actualizar sede

--- src/app/screens/sites/configTable/dialogs/sites.component.ts
+++ src/app/screens/sites/configTable/dialogs/sites.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormControl, FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ConfigTableServices } from '../services/configTable.services';
+import { NotificationService } from '@app/shared/components/notification/services/notification.service';
 
 @Component({
   selector: 'sites-app',
@@ -8,10 +10,19 @@ import { FormControl, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./sites.component.css'],
 })
 export class SitesComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { dataSite: any, subtitle:any }, private fg: FormBuilder) {}
+  id_Site: any;
+  url_Site: any;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { dataSite: any; subtitle: any },
+    private fg: FormBuilder,
+    private service: ConfigTableServices,
+    private dialog: MatDialogRef<SitesComponent>,
+    private notificationService: NotificationService
+  ) {}
 
   sites = this.data.dataSite;
-  subtitle = this.data.subtitle
+  subtitle = this.data.subtitle;
+
   // formUp!: FormGroup;
   formSities = this.fg.group({
     name: ['', Validators.required],
@@ -26,23 +37,17 @@ export class SitesComponent implements OnInit {
   }
 
   updateSites() {
-    console.log(this.formSities.value)
-    console.log(this.formSities.valid)
-    // if(this.update === true){
-    //   console.log(this.SitesForm.value);
-    //   this.addResumeService.updateRegister(this.idUpdate,this.SitesForm.value)
-    //   .then(value => {
-    //     console.log('registro actulizado con exito', value);
-    //     this.notificationService.openSimpleSnackBar(
-    //       {title: 'Hoja de vida', message: 'Hoja de vida actulizada correctamente', type: 'success'}
-    //     );
-    //   })
-    //   .catch(value => console.log(value));
-    //     this.dialogo.closeAll();
-    // }else{
-    //   this.notificationService.openSimpleSnackBar(
-    //     {title: 'Campos inválidos', message: 'La hoja de vida posee campos vacíos o inválidos', type: 'error'}
-    //   );
-    // }
+    this.id_Site = this.data.dataSite._id;
+    this.service
+      .updateDataSites(this.id_Site, this.formSities.value)
+      .then(() => {
+        this.notificationService.openSimpleSnackBar({
+          title: 'Sedes',
+          message: 'Sede Actualizada Correctamente',
+          type: 'success',
+        });
+      })
+      .catch();
+    this.dialog.close(true);
   }
 }
