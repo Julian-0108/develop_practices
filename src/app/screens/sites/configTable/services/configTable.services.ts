79ac38567bd77174a62fae66c8aae7250c8f509e
Juan Manuel Caro Vargas <juancaro@SETIAD.COM.CO>
2022-03-02 09:19:40 -0500

    :construction: Creacion del servico actualizar sede

--- src/app/screens/sites/configTable/services/configTable.services.ts
+++ src/app/screens/sites/configTable/services/configTable.services.ts
@@ -2,26 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { pluck } from 'rxjs/operators';
 import { environment } from '@environments/environment';
+import { Tables } from '../../../../shared/interfaces/profile-competences.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConfigTableServices {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-
-  async getListSites(url:string):Promise<any>{
+  async getListSites(url: string): Promise<any> {
     return await this.http
-    .get(`${environment.API_CONFIGTABLE}/${url}`)
-    .pipe(pluck('payload'))
-    .toPromise()
+      .get(`${environment.API_CONFIGTABLE}/${url}`)
+      .pipe(pluck('payload'))
+      .toPromise();
   }
 
-  async updateDataSites( _id:any, dataUp: any):Promise<any>{
+  async updateDataSites(_id: any, dataUp: any): Promise<any> {
     return await this.http
-    .put(`${environment.API_CONFIGTABLE}/venues/${_id}`, dataUp)
-    .pipe(pluck('payload'))
-    .toPromise()
+      .put(`${environment.API_CONFIGTABLE}/venues/${_id}`, dataUp)
+      .pipe(pluck('payload'))
+      .toPromise();
   }
 }
