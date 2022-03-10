6795b7edcacfd86835f1f4c87e6e6b8d71becbef
Andres Escobar Correa <andresescobar@seti.com.co>
2022-03-10 12:17:05 +0000

    :hammer: Se adicionó el nuevo enviroment de prod de sitios app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,8 @@ export const environment = {
   API_LIFE_HISTORY:  `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/v1/lifestory`,
   API_REQUISITIONS:  `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/v1/requisitions`,
   API_BASE_PROFILES : `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/v1/baseprofiles`,
-  API_SECURITY: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/v1/security-admin`
+  API_SECURITY: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/v1/security-admin`,
+  API_SITESAPP: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/v1/sitesapp`,
 };
 
 export const API_URL = 'https://sitios.setiaws.com';
