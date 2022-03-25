30120c535614c3fb783b293017ee8496b2b5d69b
Andres Duque Palacio <andresduque@seti.com.co>
2022-03-25 14:56:13 -0500

    :truck: añadir servicio obtener sitios por status verdadero

--- src/app/screens/sites/generateqr/services/sites/sites.service.ts
+++ src/app/screens/sites/generateqr/services/sites/sites.service.ts
@@ -1,7 +1,8 @@
-import { map } from 'rxjs/operators';
+import { map, pluck } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { API_URL } from 'src/environments/environment';
+// import { API_URL } from 'src/environments/environment';
+import { environment } from '@environments/environment';
 
 @Injectable({ providedIn: 'root' })
 
@@ -9,19 +10,11 @@ export class SitesService {
 
   constructor(private http: HttpClient) { }
 
-  // GET Sites list from DB
-  getSiteList() {
-    let url = API_URL + '/microsites';
-    return this.http.get(
-      url, { observe: 'response' }
-    ).pipe(map(resp => resp.body));
-  }
-
-  // GET Sites by Office ID
-  getSitesByOfficeId(body: string) {
-    const BODY = JSON.stringify({ "idOficina": body });
-    let url = API_URL + "/microsites";
-    return this.http.post(url, BODY).pipe(map(resp => resp));
+  async getListSites(): Promise<any>{
+    return await this.http
+    .get(`${environment.API_SITESAPP}/venues?status=true`)
+    .pipe(pluck('payload'))
+    .toPromise();
   }
 
 }
