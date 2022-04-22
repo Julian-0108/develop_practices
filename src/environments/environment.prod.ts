87900e4baf64bc878b066ef042d13e86db503a13
Andres Duque Palacio <andresduque@seti.com.co>
2022-04-22 09:50:28 -0500

    :recycle: reemplazar API_LIFE_HISTORY a API_MEMBERPROFILE

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   API_MASTER_INFO:  `${window.location.protocol}//${window.location.hostname}:${window.location.port}${window.location.pathname.slice(0, window.location.pathname.lastIndexOf("ui/"))}masterinfo`,
-  API_LIFE_HISTORY:  `${window.location.protocol}//${window.location.hostname}:${window.location.port}${window.location.pathname.slice(0, window.location.pathname.lastIndexOf("ui/"))}lifestory`,
+  API_MEMBERPROFILE:  `${window.location.protocol}//${window.location.hostname}:${window.location.port}${window.location.pathname.slice(0, window.location.pathname.lastIndexOf("ui/"))}memberprofile`,
   API_REQUISITIONS:  `${window.location.protocol}//${window.location.hostname}:${window.location.port}${window.location.pathname.slice(0, window.location.pathname.lastIndexOf("ui/"))}requisitions`,
   API_BASE_PROFILES : `${window.location.protocol}//${window.location.hostname}:${window.location.port}${window.location.pathname.slice(0, window.location.pathname.lastIndexOf("ui/"))}baseprofiles`,
   API_SECURITY: `${window.location.protocol}//${window.location.hostname}:${window.location.port}${window.location.pathname.slice(0, window.location.pathname.lastIndexOf("ui/"))}security-admin`,
-  API_SITESAPP: `${window.location.protocol}//${window.location.hostname}:${window.location.port}${window.location.pathname.slice(0, window.location.pathname.lastIndexOf("ui/"))}sitesapp`,
+  API_SITESAPP: `${window.location.protocol}//${window.location.hostname}:${window.location.port}${window.location.pathname.slice(0, window.location.pathname.lastIndexOf("ui/"))}sitesapp`
 };
 
 export const API_URL = 'https://sitios.setiaws.com';
